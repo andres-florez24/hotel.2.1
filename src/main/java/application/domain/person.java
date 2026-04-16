617488bd817e8d6dfed4ea1d43ef47ef7e090762
andres florez <andres.f.palacio@hotmail.com>
2026-04-15 20:50:57 -0500

    commit de prueba

---
+++ src/main/java/application/domain/person.java
@@ -0,0 +1,14 @@
+﻿package application.domain;
+
+public class person {
+
+
+    int id;
+    String name;
+    String lastName;
+    String email;
+    String password;
+    Boolean state;
+
+    
+}
